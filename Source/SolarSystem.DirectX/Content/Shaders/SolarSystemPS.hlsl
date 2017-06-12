20c720e917a199bff7b77724b94d7823e36e3c10
Nishanth Sundharesan <nishanth.reddy992@gmail.com>
2017-06-12 15:49:29 -0400

    Modified the pixel and vertex shader to include the lighting factor and light color.

--- Source/SolarSystem.DirectX/Content/Shaders/SolarSystemPS.hlsl
+++ Source/SolarSystem.DirectX/Content/Shaders/SolarSystemPS.hlsl
@@ -1,3 +1,10 @@
+cbuffer CBufferPerFrame
+{	
+	float3 LightDirection;
+	float3 LightColor;
+	float ShouldLit;
+}
+
 Texture2D ColorTexture;
 SamplerState ColorSampler;
 
@@ -5,9 +12,20 @@ struct VS_OUTPUT
 {
 	float4 Position: SV_Position;
 	float2 TextureCoordinate : TEXCOORD;
+	float3 Normal : NORMAL;
 };
 
 float4 main(VS_OUTPUT IN) : SV_TARGET
 {
-	return ColorTexture.Sample(ColorSampler, IN.TextureCoordinate);
+	float4 color = ColorTexture.Sample(ColorSampler, IN.TextureCoordinate);
+	if (ShouldLit > 0.0f)
+	{
+		float normal_dot = dot(LightDirection, IN.Normal);
+		float3 diffuse = (normal_dot > 0 ? color.rgb * normal_dot * LightColor : (float3)0);
+		return float4(diffuse, color.a);		
+	}
+	else
+	{
+		return color;
+	}		
 }

ead9f7861469cfa220ee699ff8432ab2d2c85c05
Nishanth Sundharesan <nishanth.reddy992@gmail.com>
2017-06-24 19:38:11 -0400

    Added ToggleAnimation(), added support for tilt angle and a lot more.

--- Source/SolarSystem.DirectX/CelestialObject.cpp
+++ Source/SolarSystem.DirectX/CelestialObject.cpp
@@ -7,9 +7,12 @@ using namespace DirectX;
 
 namespace SolarSystem
 {
-	CelestialObject::CelestialObject(Game& game, const shared_ptr<Camera>& camera, const wstring& texturePath, DirectX::XMFLOAT3 orbitRadius, DirectX::XMFLOAT3 scaleFactor, bool isAnimated, float rotationRate, float revolutionRate) :
-		DrawableGameComponent(game, camera), mWorldMatrix(MatrixHelper::Identity), mIndexCount(0), mTexturePath(texturePath), mOrbitRadius(orbitRadius), mObjectScale(scaleFactor), mAnimationEnabled(isAnimated), mRotationRate(rotationRate), mRevolutionRate(revolutionRate), mCurrentRotationAngle(0.0f), mCurrentRevolutionAngle(0.0f)
+	RTTI_DEFINITIONS(CelestialObject)
+
+		CelestialObject::CelestialObject(Game& game, const shared_ptr<Camera>& camera, const wstring& texturePath, const XMFLOAT3& orbitRadius, const XMFLOAT3& scaleFactor, bool isAnimated, float rotationRate, float revolutionRate, float tiltAngle, bool shouldLit) :
+		DrawableGameComponent(game, camera), mWorldMatrix(MatrixHelper::Identity), mIndexCount(0), mTexturePath(texturePath), mOrbitRadius(orbitRadius), mObjectScale(scaleFactor), mAnimationEnabled(isAnimated), mRotationRate(rotationRate), mRevolutionRate(revolutionRate), mCurrentRotationAngle(0.0f), mCurrentRevolutionAngle(0.0f), mTiltAngle(tiltAngle)
 	{
+		mPixelCBufferPerFrameData.ShouldLit = shouldLit ? 1.0f : 0.0f;
 	}
 
 	bool CelestialObject::AnimationEnabled() const
@@ -22,7 +25,12 @@ namespace SolarSystem
 		mAnimationEnabled = enabled;
 	}
 
-	XMMATRIX CelestialObject::PositionMatrix() const
+	void CelestialObject::ToggleAnimation()
+	{
+		mAnimationEnabled = !mAnimationEnabled;
+	}
+
+	const XMFLOAT4X4& CelestialObject::PositionMatrix() const
 	{
 		return mPositionMatrix;
 	}
@@ -43,7 +51,8 @@ namespace SolarSystem
 		D3D11_INPUT_ELEMENT_DESC inputElementDescriptions[] =
 		{
 			{ "POSITION", 0, DXGI_FORMAT_R32G32B32A32_FLOAT, 0, 0, D3D11_INPUT_PER_VERTEX_DATA, 0 },
-			{ "TEXCOORD", 0, DXGI_FORMAT_R32G32_FLOAT, 0, D3D11_APPEND_ALIGNED_ELEMENT, D3D11_INPUT_PER_VERTEX_DATA, 0 }
+			{ "TEXCOORD", 0, DXGI_FORMAT_R32G32_FLOAT, 0, D3D11_APPEND_ALIGNED_ELEMENT, D3D11_INPUT_PER_VERTEX_DATA, 0 },
+			{ "NORMAL", 0, DXGI_FORMAT_R32G32B32_FLOAT, 0, D3D11_APPEND_ALIGNED_ELEMENT, D3D11_INPUT_PER_VERTEX_DATA, 0 }
 		};
 
 		ThrowIfFailed(mGame->Direct3DDevice()->CreateInputLayout(inputElementDescriptions, ARRAYSIZE(inputElementDescriptions), &compiledVertexShader[0], compiledVertexShader.size(), mInputLayout.ReleaseAndGetAddressOf()), "ID3D11Device::CreateInputLayout() failed.");
@@ -52,42 +61,38 @@ namespace SolarSystem
 		Library::Model model("Content\\Models\\Sphere.obj.bin");
 
 		// Create vertex and index buffers for the model
-		Mesh* mesh = model.Meshes().at(0).get();
+		Library::Mesh* mesh = model.Meshes().at(0).get();
 		CreateVertexBuffer(*mesh, mVertexBuffer.ReleaseAndGetAddressOf());
 		mesh->CreateIndexBuffer(*mGame->Direct3DDevice(), mIndexBuffer.ReleaseAndGetAddressOf());
 		mIndexCount = static_cast<uint32_t>(mesh->Indices().size());
 
+		// Create constant buffers
 		D3D11_BUFFER_DESC constantBufferDesc = { 0 };
-		constantBufferDesc.ByteWidth = sizeof(CBufferPerObject);
+		constantBufferDesc.ByteWidth = sizeof(VertexCBufferPerObject);
 		constantBufferDesc.BindFlags = D3D11_BIND_CONSTANT_BUFFER;
-		ThrowIfFailed(mGame->Direct3DDevice()->CreateBuffer(&constantBufferDesc, nullptr, mConstantBuffer.ReleaseAndGetAddressOf()), "ID3D11Device::CreateBuffer() failed.");
+		ThrowIfFailed(mGame->Direct3DDevice()->CreateBuffer(&constantBufferDesc, nullptr, mVertexCBufferPerObject.ReleaseAndGetAddressOf()), "ID3D11Device::CreateBuffer() failed.");
+
+		constantBufferDesc.ByteWidth = sizeof(PixelCBufferPerFrame);
+		ThrowIfFailed(mGame->Direct3DDevice()->CreateBuffer(&constantBufferDesc, nullptr, mPixelCBufferPerFrame.ReleaseAndGetAddressOf()), "ID3D11Device::CreateBuffer() failed.");
+
+		// Initialize cbuffers with default data
+		mGame->Direct3DDeviceContext()->UpdateSubresource(mPixelCBufferPerFrame.Get(), 0, nullptr, &mPixelCBufferPerFrameData, 0, 0);
 
 		// Load a texture		
 		ThrowIfFailed(CreateWICTextureFromFile(mGame->Direct3DDevice(), mGame->Direct3DDeviceContext(), mTexturePath.c_str(), nullptr, mColorTexture.ReleaseAndGetAddressOf()), "CreateWICTextureFromFile() failed.");
-	}
 
-	void CelestialObject::Update(const GameTime& gameTime)
-	{
-		if (mAnimationEnabled)
-		{
-			mCurrentRotationAngle += gameTime.ElapsedGameTimeSeconds().count() * mRotationRate;
-			mCurrentRevolutionAngle += gameTime.ElapsedGameTimeSeconds().count() * mRevolutionRate;
-			mPositionMatrix = XMMatrixTranslation(mOrbitRadius.x, mOrbitRadius.y, mOrbitRadius.z) * XMMatrixRotationY(mCurrentRevolutionAngle);
-			XMMATRIX translatedMatrix = XMMatrixScaling(mObjectScale.x, mObjectScale.y, mObjectScale.y) * XMMatrixRotationY(mCurrentRotationAngle) * mPositionMatrix;
-			XMStoreFloat4x4(&mWorldMatrix, translatedMatrix);
-		}
+		mDirectionalLight = make_unique<DirectionalLight>(*mGame);
+		mPixelCBufferPerFrameData.LightDirection = mDirectionalLight->DirectionToLight();
 	}
 
 	void CelestialObject::Draw(const GameTime& gameTime)
 	{
 		UNREFERENCED_PARAMETER(gameTime);
-		assert(mCamera != nullptr);
-
 		ID3D11DeviceContext* direct3DDeviceContext = mGame->Direct3DDeviceContext();
 		direct3DDeviceContext->IASetPrimitiveTopology(D3D11_PRIMITIVE_TOPOLOGY_TRIANGLELIST);
 		direct3DDeviceContext->IASetInputLayout(mInputLayout.Get());
 
-		UINT stride = sizeof(VertexPositionTexture);
+		UINT stride = sizeof(VertexPositionTextureNormal);
 		UINT offset = 0;
 		direct3DDeviceContext->IASetVertexBuffers(0, 1, mVertexBuffer.GetAddressOf(), &stride, &offset);
 		direct3DDeviceContext->IASetIndexBuffer(mIndexBuffer.Get(), DXGI_FORMAT_R32_UINT, 0);
@@ -98,10 +103,12 @@ namespace SolarSystem
 		XMMATRIX worldMatrix = XMLoadFloat4x4(&mWorldMatrix);
 		XMMATRIX wvp = worldMatrix * mCamera->ViewProjectionMatrix();
 		wvp = XMMatrixTranspose(wvp);
-		XMStoreFloat4x4(&mCBufferPerObject.WorldViewProjection, wvp);
+		XMStoreFloat4x4(&mVertexCBufferPerObjectData.WorldViewProjection, wvp);
+		XMStoreFloat4x4(&mVertexCBufferPerObjectData.World, XMMatrixTranspose(worldMatrix));
 
-		direct3DDeviceContext->UpdateSubresource(mConstantBuffer.Get(), 0, nullptr, &mCBufferPerObject, 0, 0);
-		direct3DDeviceContext->VSSetConstantBuffers(0, 1, mConstantBuffer.GetAddressOf());
+		direct3DDeviceContext->UpdateSubresource(mVertexCBufferPerObject.Get(), 0, nullptr, &mVertexCBufferPerObjectData, 0, 0);
+		direct3DDeviceContext->VSSetConstantBuffers(0, 1, mVertexCBufferPerObject.GetAddressOf());
+		direct3DDeviceContext->PSSetConstantBuffers(0, 1, mPixelCBufferPerFrame.GetAddressOf());
 
 		direct3DDeviceContext->PSSetShaderResources(0, 1, mColorTexture.GetAddressOf());
 		direct3DDeviceContext->PSSetSamplers(0, 1, SamplerStates::TrilinearWrap.GetAddressOf());
@@ -112,23 +119,33 @@ namespace SolarSystem
 	void CelestialObject::CreateVertexBuffer(const Mesh& mesh, ID3D11Buffer** vertexBuffer) const
 	{
 		const vector<XMFLOAT3>& sourceVertices = mesh.Vertices();
-		const auto& sourceUVs = mesh.TextureCoordinates().at(0);
+		const vector<XMFLOAT3>* const textureCoordinates = mesh.TextureCoordinates().at(0);
+		assert(textureCoordinates->size() == sourceVertices.size());
+		const vector<XMFLOAT3>& normals = mesh.Normals();
+		assert(normals.size() == sourceVertices.size());
 
-		vector<VertexPositionTexture> vertices;
+		vector<VertexPositionTextureNormal> vertices;
 		vertices.reserve(sourceVertices.size());
 		for (size_t i = 0; i < sourceVertices.size(); i++)
 		{
 			const XMFLOAT3& position = sourceVertices.at(i);
-			const XMFLOAT3& uv = sourceUVs->at(i);
-			vertices.push_back(VertexPositionTexture(XMFLOAT4(position.x, position.y, position.z, 1.0f), XMFLOAT2(uv.x, uv.y)));
+			const XMFLOAT3& uv = textureCoordinates->at(i);
+			const XMFLOAT3& normal = normals.at(i);
+			vertices.push_back(VertexPositionTextureNormal(XMFLOAT4(position.x, position.y, position.z, 1.0f), XMFLOAT2(uv.x, uv.y), normal));
 		}
+
 		D3D11_BUFFER_DESC vertexBufferDesc = { 0 };
-		vertexBufferDesc.ByteWidth = sizeof(VertexPositionTexture) * static_cast<uint32_t>(vertices.size());
+		vertexBufferDesc.ByteWidth = sizeof(VertexPositionTextureNormal) * static_cast<UINT>(vertices.size());
 		vertexBufferDesc.Usage = D3D11_USAGE_IMMUTABLE;
 		vertexBufferDesc.BindFlags = D3D11_BIND_VERTEX_BUFFER;
 
 		D3D11_SUBRESOURCE_DATA vertexSubResourceData = { 0 };
 		vertexSubResourceData.pSysMem = &vertices[0];
 		ThrowIfFailed(mGame->Direct3DDevice()->CreateBuffer(&vertexBufferDesc, &vertexSubResourceData, vertexBuffer), "ID3D11Device::CreateBuffer() failed.");
+	}
+
+	void CelestialObject::UpdateDirectionalLight(const GameTime& gameTime)
+	{
+		UNREFERENCED_PARAMETER(gameTime);
 	}
 }
--- Source/SolarSystem.DirectX/CelestialObject.h
+++ Source/SolarSystem.DirectX/CelestialObject.h
@@ -11,14 +11,16 @@ namespace Library
 {
 	class Camera;
 	class Mesh;
+	class DirectionalLight;
 }
 
 namespace SolarSystem
 {
 	class CelestialObject : public Library::DrawableGameComponent
 	{
+		RTTI_DECLARATIONS(CelestialObject, Library::DrawableGameComponent)
 	public:
-		CelestialObject(Library::Game& game, const std::shared_ptr<Library::Camera>& camera, const std::wstring& texturePath, DirectX::XMFLOAT3 orbitRadius, DirectX::XMFLOAT3 scaleFactor, bool isAnimated, float rotationRate, float revolutionRate);
+		CelestialObject(Library::Game& game, const std::shared_ptr<Library::Camera>& camera, const std::wstring& texturePath, const DirectX::XMFLOAT3& orbitRadius, const DirectX::XMFLOAT3& scaleFactor, bool isAnimated, float rotationRate, float revolutionRate, float tiltAngle, bool shouldLit);
 
 		CelestialObject(const CelestialObject&) = default;
 		CelestialObject& operator=(const CelestialObject&) = default;
@@ -28,39 +30,62 @@ namespace SolarSystem
 
 		bool AnimationEnabled() const;
 		void SetAnimationEnabled(bool enabled);
+		void ToggleAnimation();
 
-		DirectX::XMMATRIX PositionMatrix() const;
+		const DirectX::XMFLOAT4X4& PositionMatrix() const;
 		virtual void Initialize() override;
-		virtual void Update(const Library::GameTime& gameTime) override;
 		virtual void Draw(const Library::GameTime& gameTime) override;
 
-	private:		
-		struct CBufferPerObject
+	private:
+		struct VertexCBufferPerObject
 		{
 			DirectX::XMFLOAT4X4 WorldViewProjection;
+			DirectX::XMFLOAT4X4 World;
 
-			CBufferPerObject() = default;
-			CBufferPerObject(const DirectX::XMFLOAT4X4& wvp) : WorldViewProjection(wvp) { }
+			VertexCBufferPerObject() = default;
+			VertexCBufferPerObject(const DirectX::XMFLOAT4X4& wvp, const DirectX::XMFLOAT4X4& world) : WorldViewProjection(wvp), World(world) { }
+		};
+
+		struct PixelCBufferPerFrame
+		{
+			DirectX::XMFLOAT3 LightDirection;
+			float Padding;
+			DirectX::XMFLOAT3 LightColor;
+			float ShouldLit;
+
+			PixelCBufferPerFrame() :
+				LightDirection(0.0f, 0.0f, 1.0f), LightColor(DirectX::Colors::White) { }
+
+			PixelCBufferPerFrame(const DirectX::XMFLOAT3& lightDirection, const DirectX::XMFLOAT3& lightColor) :
+				LightDirection(lightDirection), LightColor(lightColor) { }
 		};
 
 		void CreateVertexBuffer(const Library::Mesh& mesh, ID3D11Buffer** vertexBuffer) const;
 
-		DirectX::XMMATRIX mPositionMatrix;
-		CBufferPerObject mCBufferPerObject;
 		Microsoft::WRL::ComPtr<ID3D11VertexShader> mVertexShader;
 		Microsoft::WRL::ComPtr<ID3D11PixelShader> mPixelShader;
 		Microsoft::WRL::ComPtr<ID3D11InputLayout> mInputLayout;
 		Microsoft::WRL::ComPtr<ID3D11Buffer> mVertexBuffer;
 		Microsoft::WRL::ComPtr<ID3D11Buffer> mIndexBuffer;
-		Microsoft::WRL::ComPtr<ID3D11Buffer> mConstantBuffer;
 		Microsoft::WRL::ComPtr<ID3D11ShaderResourceView> mColorTexture;
 		std::uint32_t mIndexCount;
 
 	protected:
+		virtual void UpdateDirectionalLight(const Library::GameTime& gameTime);
+
+		VertexCBufferPerObject mVertexCBufferPerObjectData;
+		PixelCBufferPerFrame mPixelCBufferPerFrameData;
+
+		Microsoft::WRL::ComPtr<ID3D11Buffer> mVertexCBufferPerObject;
+		Microsoft::WRL::ComPtr<ID3D11Buffer> mPixelCBufferPerFrame;
+
+		std::unique_ptr<Library::DirectionalLight> mDirectionalLight;
+		DirectX::XMFLOAT4X4 mPositionMatrix;
 		DirectX::XMFLOAT4X4 mWorldMatrix;
 		float mCurrentRotationAngle;
 		float mCurrentRevolutionAngle;
 		bool mAnimationEnabled;
+		float mTiltAngle;
 		float mRotationRate;
 		float mRevolutionRate;
 		std::wstring mTexturePath;

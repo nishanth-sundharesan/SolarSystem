acdd34b8240614de973042aa4283e7d3be0481e9
Nishanth Sundharesan <nishanth.reddy992@gmail.com>
2017-06-24 19:35:47 -0400

    Added Update() and added an additional parameter tiltangle to the constructor.

--- Source/SolarSystem.DirectX/Star.cpp
+++ Source/SolarSystem.DirectX/Star.cpp
@@ -3,15 +3,22 @@
 
 using namespace std;
 using namespace Library;
+
 using namespace DirectX;
 using namespace Rendering;
-
 namespace SolarSystem
 {
-	Star::Star(Game& game, const shared_ptr<Library::Camera>& camera, const wstring& texturePath, const XMFLOAT3& position, const XMFLOAT3& scaleFactor, bool isAnimated, float rotationRate, float revolutionRate)
-		:CelestialObject(game, camera, texturePath, position, scaleFactor, isAnimated, rotationRate, revolutionRate, false), mPosition(position)
+	Star::Star(Game& game, const shared_ptr<Library::Camera>& camera, const wstring& texturePath, const XMFLOAT3& position, const XMFLOAT3& scaleFactor, bool isAnimated, float rotationRate, float revolutionRate, float tiltAngle)
+		:CelestialObject(game, camera, texturePath, position, scaleFactor, isAnimated, rotationRate, revolutionRate, tiltAngle, false), mPosition(position)
 	{
-		mPositionInXMVector = XMLoadFloat3(&mPosition);
+		//mPositionInXMVector = XMLoadFloat3(&mPosition);
+	}
+
+	void Star::Update(const Library::GameTime& gameTime)
+	{
+		UNREFERENCED_PARAMETER(gameTime);
+		XMMATRIX translatedMatrix = XMMatrixScaling(mObjectScale.x, mObjectScale.y, mObjectScale.y);
+		XMStoreFloat4x4(&mWorldMatrix, translatedMatrix);
 	}
 
 	const XMFLOAT3& Star::Position() const
@@ -19,8 +26,8 @@ namespace SolarSystem
 		return mPosition;
 	}
 
-	CXMVECTOR Star::PositionInXMVector() const
+	XMVECTOR Star::PositionInXMVector() const
 	{
-		return mPositionInXMVector;
+		return XMLoadFloat3(&mPosition);
 	}
 }
--- Source/SolarSystem.DirectX/Star.h
+++ Source/SolarSystem.DirectX/Star.h
@@ -6,17 +6,17 @@ namespace SolarSystem
 	class Star final : public CelestialObject
 	{
 	public:
-		Star(Library::Game& game, const std::shared_ptr<Library::Camera>& camera, const std::wstring& texturePath, const DirectX::XMFLOAT3& position, const DirectX::XMFLOAT3& scaleFactor, bool isAnimated, float rotationRate, float revolutionRate);
+		Star(Library::Game& game, const std::shared_ptr<Library::Camera>& camera, const std::wstring& texturePath, const DirectX::XMFLOAT3& position, const DirectX::XMFLOAT3& scaleFactor, bool isAnimated, float rotationRate, float revolutionRate, float tiltAngle);
 		Star(const Star&) = default;
 		Star& operator=(const Star&) = default;
 		Star(Star&&) = default;
 		Star& operator=(Star&&) = default;
 		virtual ~Star() = default;
 
+		virtual void Update(const Library::GameTime& gameTime) override;
 		const DirectX::XMFLOAT3& Position() const;
-		DirectX::CXMVECTOR PositionInXMVector() const;
-	private:
-		DirectX::XMVECTOR mPositionInXMVector;
+		DirectX::XMVECTOR PositionInXMVector() const;
+	private:		
 		DirectX::XMFLOAT3 mPosition;		
 	};
 }

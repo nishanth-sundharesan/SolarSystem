55ff7e4647e337789135b0b82ccb0ab64f961f57
Nishanth Sundharesan <nishanth.reddy992@gmail.com>
2017-06-25 23:13:28 -0400

    Added all the planets and satellites. Added support for toggling of animations.

--- Source/SolarSystem.DirectX/RenderingGame.cpp
+++ Source/SolarSystem.DirectX/RenderingGame.cpp
@@ -35,48 +35,83 @@ namespace Rendering
 		mComponents.push_back(mCamera);
 		mServices.AddService(Camera::TypeIdClass(), mCamera.get());
 
-		mOrbit = make_shared<Orbit>(*this, mCamera, 40, 200);
-		mComponents.push_back(mOrbit);
+#pragma region Orbits		
+		mComponents.push_back(make_shared<Orbit>(*this, mCamera, MercuryConstants::sNumberOfVertices, static_cast<uint32_t>(MercuryConstants::sOrbitRadius.x)));
 
-		mSun = make_shared<Star>(*this, mCamera, SunConstants::sTexturePath, SunConstants::sOrbitRadius, SunConstants::sScale, false, SunConstants::sRotationRate, SunConstants::sRevolutionRate);
+		mComponents.push_back(make_shared<Orbit>(*this, mCamera, VenusConstants::sNumberOfVertices, static_cast<uint32_t>(VenusConstants::sOrbitRadius.x)));
+
+		mComponents.push_back(make_shared<Orbit>(*this, mCamera, EarthConstants::sNumberOfVertices, static_cast<uint32_t>(EarthConstants::sOrbitRadius.x)));
+
+		mComponents.push_back(make_shared<Orbit>(*this, mCamera, MarsConstants::sNumberOfVertices, static_cast<uint32_t>(MarsConstants::sOrbitRadius.x)));
+
+		mComponents.push_back(make_shared<Orbit>(*this, mCamera, JupiterConstants::sNumberOfVertices, static_cast<uint32_t>(JupiterConstants::sOrbitRadius.x)));
+
+		mComponents.push_back(make_shared<Orbit>(*this, mCamera, SaturnConstants::sNumberOfVertices, static_cast<uint32_t>(SaturnConstants::sOrbitRadius.x)));
+
+		mComponents.push_back(make_shared<Orbit>(*this, mCamera, UranusConstants::sNumberOfVertices, static_cast<uint32_t>(UranusConstants::sOrbitRadius.x)));
+
+		mComponents.push_back(make_shared<Orbit>(*this, mCamera, NeptuneConstants::sNumberOfVertices, static_cast<uint32_t>(NeptuneConstants::sOrbitRadius.x)));
+
+		mComponents.push_back(make_shared<Orbit>(*this, mCamera, PlutoConstants::sNumberOfVertices, static_cast<uint32_t>(PlutoConstants::sOrbitRadius.x)));
+#pragma endregion
+
+#pragma region Stars
+
+		shared_ptr<SolarSystem::Star> mSun = make_shared<Star>(*this, mCamera, SunConstants::sTexturePath, SunConstants::sOrbitRadius, SunConstants::sScale, false, SunConstants::sRotationRate, SunConstants::sRevolutionRate, SunConstants::sTiltAngle);
+		mStars.push_back(mSun);
 		mComponents.push_back(mSun);
+#pragma endregion
 
-		mMercury = make_shared<Planet>(*this, mCamera, MercuryConstants::sTexturePath, MercuryConstants::sOrbitRadius, MercuryConstants::sScale, true, MercuryConstants::sRotationRate, MercuryConstants::sRevolutionRate, mSun);
+#pragma region Planets
+		shared_ptr<SolarSystem::Planet> mMercury = make_shared<Planet>(*this, mCamera, MercuryConstants::sTexturePath, MercuryConstants::sOrbitRadius, MercuryConstants::sScale, true, MercuryConstants::sRotationRate, MercuryConstants::sRevolutionRate, MercuryConstants::sTiltAngle, mSun);
+		mPlanets.push_back(mMercury);
 		mComponents.push_back(mMercury);
 
-		mVenus = make_shared<Planet>(*this, mCamera, VenusConstants::sTexturePath, VenusConstants::sOrbitRadius, VenusConstants::sScale, true, VenusConstants::sRotationRate, VenusConstants::sRevolutionRate, mSun);
+		shared_ptr<SolarSystem::Planet> mVenus = make_shared<Planet>(*this, mCamera, VenusConstants::sTexturePath, VenusConstants::sOrbitRadius, VenusConstants::sScale, true, VenusConstants::sRotationRate, VenusConstants::sRevolutionRate, VenusConstants::sTiltAngle, mSun);
+		mPlanets.push_back(mVenus);
 		mComponents.push_back(mVenus);
 
-		mEarth = make_shared<Planet>(*this, mCamera, EarthConstants::sTexturePath, EarthConstants::sOrbitRadius, EarthConstants::sScale, true, EarthConstants::sRotationRate, EarthConstants::sRevolutionRate, mSun);
+		shared_ptr<SolarSystem::Planet> mEarth = make_shared<Planet>(*this, mCamera, EarthConstants::sTexturePath, EarthConstants::sOrbitRadius, EarthConstants::sScale, true, EarthConstants::sRotationRate, EarthConstants::sRevolutionRate, EarthConstants::sTiltAngle, mSun);
+		mPlanets.push_back(mEarth);
 		mComponents.push_back(mEarth);
 
-		mMoon = make_shared<Satellite>(*this, mCamera, MoonConstants::sTexturePath, MoonConstants::sOrbitRadius, MoonConstants::sScale, true, MoonConstants::sRotationRate, MoonConstants::sRevolutionRate, mEarth);
-		mComponents.push_back(mMoon);
-
-		mMars = make_shared<Planet>(*this, mCamera, MarsConstants::sTexturePath, MarsConstants::sOrbitRadius, MarsConstants::sScale, true, MarsConstants::sRotationRate, MarsConstants::sRevolutionRate, mSun);
+		shared_ptr<SolarSystem::Planet> mMars = make_shared<Planet>(*this, mCamera, MarsConstants::sTexturePath, MarsConstants::sOrbitRadius, MarsConstants::sScale, true, MarsConstants::sRotationRate, MarsConstants::sRevolutionRate, MarsConstants::sTiltAngle, mSun);
+		mPlanets.push_back(mMars);
 		mComponents.push_back(mMars);
 
-		mJupiter = make_shared<Planet>(*this, mCamera, JupiterConstants::sTexturePath, JupiterConstants::sOrbitRadius, JupiterConstants::sScale, true, JupiterConstants::sRotationRate, JupiterConstants::sRevolutionRate, mSun);
+		shared_ptr<SolarSystem::Planet> mJupiter = make_shared<Planet>(*this, mCamera, JupiterConstants::sTexturePath, JupiterConstants::sOrbitRadius, JupiterConstants::sScale, true, JupiterConstants::sRotationRate, JupiterConstants::sRevolutionRate, JupiterConstants::sTiltAngle, mSun);
+		mPlanets.push_back(mJupiter);
 		mComponents.push_back(mJupiter);
 
-		mSaturn = make_shared<Planet>(*this, mCamera, SaturnConstants::sTexturePath, SaturnConstants::sOrbitRadius, SaturnConstants::sScale, true, SaturnConstants::sRotationRate, SaturnConstants::sRevolutionRate, mSun);
+		shared_ptr<SolarSystem::Planet> mSaturn = make_shared<Planet>(*this, mCamera, SaturnConstants::sTexturePath, SaturnConstants::sOrbitRadius, SaturnConstants::sScale, true, SaturnConstants::sRotationRate, SaturnConstants::sRevolutionRate, SaturnConstants::sTiltAngle, mSun);
+		mPlanets.push_back(mSaturn);
 		mComponents.push_back(mSaturn);
 
-		mUranus = make_shared<Planet>(*this, mCamera, UranusConstants::sTexturePath, UranusConstants::sOrbitRadius, UranusConstants::sScale, true, UranusConstants::sRotationRate, UranusConstants::sRevolutionRate, mSun);
+		shared_ptr<SolarSystem::Planet> mUranus = make_shared<Planet>(*this, mCamera, UranusConstants::sTexturePath, UranusConstants::sOrbitRadius, UranusConstants::sScale, true, UranusConstants::sRotationRate, UranusConstants::sRevolutionRate, UranusConstants::sTiltAngle, mSun);
+		mPlanets.push_back(mUranus);
 		mComponents.push_back(mUranus);
 
-		mNeptune = make_shared<Planet>(*this, mCamera, NeptuneConstants::sTexturePath, NeptuneConstants::sOrbitRadius, NeptuneConstants::sScale, true, NeptuneConstants::sRotationRate, NeptuneConstants::sRevolutionRate, mSun);
+		shared_ptr<SolarSystem::Planet> mNeptune = make_shared<Planet>(*this, mCamera, NeptuneConstants::sTexturePath, NeptuneConstants::sOrbitRadius, NeptuneConstants::sScale, true, NeptuneConstants::sRotationRate, NeptuneConstants::sRevolutionRate, NeptuneConstants::sTiltAngle, mSun);
+		mPlanets.push_back(mNeptune);
 		mComponents.push_back(mNeptune);
 
-		mPluto = make_shared<Planet>(*this, mCamera, PlutoConstants::sTexturePath, PlutoConstants::sOrbitRadius, PlutoConstants::sScale, true, PlutoConstants::sRotationRate, PlutoConstants::sRevolutionRate, mSun);
+		shared_ptr<SolarSystem::Planet> mPluto = make_shared<Planet>(*this, mCamera, PlutoConstants::sTexturePath, PlutoConstants::sOrbitRadius, PlutoConstants::sScale, true, PlutoConstants::sRotationRate, PlutoConstants::sRevolutionRate, PlutoConstants::sTiltAngle, mSun);
+		mPlanets.push_back(mPluto);
 		mComponents.push_back(mPluto);
+#pragma endregion
+
+#pragma region Satellites
+		shared_ptr<SolarSystem::Satellite> mMoon = make_shared<Satellite>(*this, mCamera, MoonConstants::sTexturePath, MoonConstants::sOrbitRadius, MoonConstants::sScale, true, MoonConstants::sRotationRate, MoonConstants::sRevolutionRate, MoonConstants::sTiltAngle, mEarth);
+		mSatellites.push_back(mMoon);
+		mComponents.push_back(mMoon);
+#pragma endregion
 
 		Game::Initialize();
 
 		mRenderStateHelper = make_unique<RenderStateHelper>(*this);
 		mFpsComponent = make_shared<FpsComponent>(*this);
 		mFpsComponent->Initialize();
-		mCamera->SetPosition(0.0f, 5.0f, 100.0f);
+		mCamera->SetPosition(0.0f, 50.0f, 350.0f);
 	}
 
 	void RenderingGame::Update(const GameTime &gameTime)
@@ -87,7 +122,23 @@ namespace Rendering
 		{
 			Exit();
 		}
+		if (mKeyboard->WasKeyPressedThisFrame(Keys::Space))
+		{
+			for (auto celestialObject : mStars)
+			{
+				celestialObject->ToggleAnimation();
+			}
 
+			for (auto celestialObject : mPlanets)
+			{
+				celestialObject->ToggleAnimation();
+			}
+
+			for (auto celestialObject : mSatellites)
+			{
+				celestialObject->ToggleAnimation();
+			}
+		}
 		Game::Update(gameTime);
 	}
 
--- Source/SolarSystem.DirectX/RenderingGame.h
+++ Source/SolarSystem.DirectX/RenderingGame.h
@@ -11,51 +11,42 @@ namespace Library
 	class FpsComponent;
 	class Camera;
 	class RenderStateHelper;
-	class Grid;	
+	class Grid;
 }
 
 namespace SolarSystem
-{	
+{
 	class CelestialObject;
 	class Star;
-	class Orbit;
+	class Planet;
+	class Satellite;
 }
 
 namespace Rendering
-{	
-    class RenderingGame : public Library::Game
-    {
-    public:
+{
+	class RenderingGame : public Library::Game
+	{
+	public:
 		RenderingGame(std::function<void*()> getWindowCallback, std::function<void(SIZE&)> getRenderTargetSizeCallback);
 
 		virtual void Initialize() override;
 		virtual void Update(const Library::GameTime& gameTime) override;
-		virtual void Draw(const Library::GameTime& gameTime) override;		
+		virtual void Draw(const Library::GameTime& gameTime) override;
 		virtual void Shutdown() override;
 
 		void Exit();
 
-    private:
+	private:
 		static const DirectX::XMVECTORF32 BackgroundColor;
 
 		std::shared_ptr<Library::KeyboardComponent> mKeyboard;
 		std::shared_ptr<Library::MouseComponent> mMouse;
 		std::shared_ptr<Library::FpsComponent> mFpsComponent;
 		std::shared_ptr<Library::Camera> mCamera;
-		std::unique_ptr<Library::RenderStateHelper> mRenderStateHelper;
-		std::shared_ptr<Library::Grid> mGrid;
+		std::unique_ptr<Library::RenderStateHelper> mRenderStateHelper;		
 
-		std::shared_ptr<SolarSystem::Orbit> mOrbit;
-		std::shared_ptr<SolarSystem::Star> mSun;
-		std::shared_ptr<SolarSystem::CelestialObject> mMercury;
-		std::shared_ptr<SolarSystem::CelestialObject> mVenus;
-		std::shared_ptr<SolarSystem::CelestialObject> mEarth;
-		std::shared_ptr<SolarSystem::CelestialObject> mMars;
-		std::shared_ptr<SolarSystem::CelestialObject> mJupiter;
-		std::shared_ptr<SolarSystem::CelestialObject> mSaturn;
-		std::shared_ptr<SolarSystem::CelestialObject> mUranus;
-		std::shared_ptr<SolarSystem::CelestialObject> mNeptune;
-		std::shared_ptr<SolarSystem::CelestialObject> mPluto;
-		std::shared_ptr<SolarSystem::CelestialObject> mMoon;
-    };
+		std::vector<std::shared_ptr<SolarSystem::Star>> mStars;
+		std::vector<std::shared_ptr<SolarSystem::Planet>> mPlanets;
+		std::vector<std::shared_ptr<SolarSystem::Satellite>> mSatellites;			
+	};
 }
